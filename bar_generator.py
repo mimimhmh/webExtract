d3dc5ca5e986b9efc4198e4167eb8dca6fe8300a
mimimhmh <mimimhmh@gmail.com>
2016-08-17 02:14:02 +1200

    Bar Chart setting
    create bar chart

--- bar_generator.py
+++ bar_generator.py
@@ -1,29 +1,38 @@
 import plotly.plotly as py
 import plotly.graph_objs as go
 from collections import Counter
-from games import Classification
+from games import Game, Classification
+from html_analyse_tool import MyTool
 
 py.sign_in('mimimhmh@gmail.com', '8tapwyxkuv')
 
+class BarTool(object):
 
-games = h2g.get_games(urls)
-c = Counter()
-for game in games:
-    print(game.name)
-    c[game.classification] = c[game.classification] + 1
+    def show_bar(self):
+        mt = MyTool()
+        games = mt.analyse()
+        c = Counter()
+        for game in games:
+            print(game.name)
+            c[game.classification] = c[game.classification] + 1
 
-count_G = c[Classification.G.value]
-count_PG = c[Classification.PG.value]
-count_M = c[Classification.M.value]
-count_R13 = c[Classification.R13.value]
-count_R16 = c[Classification.R16.value]
-count_R18 = c[Classification.R18.value]
-count_TBC = c[Classification.TBC.value]
-count_UNDEFINED = c[Classification.UNDEFINED.value]
+        count_G = c[Classification.G.value]
+        count_PG = c[Classification.PG.value]
+        count_M = c[Classification.M.value]
+        count_R13 = c[Classification.R13.value]
+        count_R16 = c[Classification.R16.value]
+        count_R18 = c[Classification.R18.value]
+        count_TBC = c[Classification.TBC.value]
+        count_UNDEFINED = c[Classification.UNDEFINED.value]
 
-data = [go.Bar(
-    x=['G', 'PG', 'M', 'R13', 'R16', 'R18', 'TBC', 'UNDEFINED'],
-    y=[count_G, count_PG, count_M, count_R13, count_R16, count_R18, count_TBC, count_UNDEFINED]
-)]
+        data = [go.Bar(
+            x=['G', 'PG', 'M', 'R13', 'R16', 'R18', 'TBC', 'UNDEFINED'],
+            y=[count_G, count_PG, count_M, count_R13, count_R16, count_R18, count_TBC, count_UNDEFINED]
+        )]
 
-py.plot(data, filename='basic-bar')
+        py.plot(data, filename='basic-bar')
+
+
+if __name__ == '__main__':
+    bt = BarTool()
+    bt.show_bar()
--- html_analyse_tool.py
+++ html_analyse_tool.py
@@ -10,14 +10,15 @@ class MyTool(object):
             #got how many games in the content
             count = len(selector.xpath('//tr[@class="game"]'))
             for i in range(count):
-                gid = selector.xpath('//td[@class="gid"]/text()')[0]
-                title = selector.xpath('//td[@class="title"]/text()')[0]
-                price = selector.xpath('//td[@class="price"]/text()')[0]
-                cls = selector.xpath('//td[@class="cls"]/text()')[0]
-                release_date = selector.xpath('//td[@class="release_date"]/text()')[0]
-                in_stock = selector.xpath('//td[@class="release_date"]/text()')[0]
+                gid = selector.xpath('//td[@class="gid"]/text()')[i]
+                title = selector.xpath('//td[@class="title"]/text()')[i]
+                price = selector.xpath('//td[@class="price"]/text()')[i]
+                cls = selector.xpath('//td[@class="cls"]/text()')[i]
+                release_date = selector.xpath('//td[@class="release_date"]/text()')[i]
+                in_stock = selector.xpath('//td[@class="release_date"]/text()')[i]
                 game = Game(gid, title, price, cls, release_date, in_stock)
                 games.append(game)
+                #print(title)
         return games
 
 if __name__ == '__main__':
